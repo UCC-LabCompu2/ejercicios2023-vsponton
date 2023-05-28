89ac0767ac26f169d4adc9f0fa3670504d85d5b7
vsponton <2146222@ucc.edu.ar>
2023-05-28 09:47:51 -0300

    Se realizaron los ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,29 +6,41 @@
  * @return
  */
 function cambiarUnidades(id,valor) {
-    if (isNaN(valor)){
-        alert("Se agrego un valor invalido");
-        document.lasUnidades.unid_metro.value="";
-        document.lasUnidades.unid_pulgada.value ="";
-        document.lasUnidades.unid_pie.value ="";
-        document.lasUnidades.unid_yarda.value ="";
-    }else if(id=="metro"){
-        document.lasUnidades.unid_pulgada.value = 39.3701*valor;
-        document.lasUnidades.unid_pie.value = 3.28084*valor;
-        document.lasUnidades.unid_yarda.value = 1.09361*valor;
-    }else if(id=="pulgada"){
-        document.lasUnidades.unid_metro.value = 0.0254*valor;
-        document.lasUnidades.unid_pie.value = 0.0833333 *valor;
-        document.lasUnidades.unid_yarda.value = 0.0277778 *valor;
-    }else if(id=="yarda"){
-        document.lasUnidades.unid_metro.value= 0.9144*valor;
-        document.lasUnidades.unid_pie.value = 3*valor;
-        document.lasUnidades.unid_pulgada.value = 36*valor;
-    }else if(id=="pie"){
-        document.lasUnidades.metro.value = 0.3048*valor;
-        document.lasUnidades.pulgada.value = 12*valor;
-        document.lasUnidades.yarda.value = 0.333333*valor
+    var metro,pulgada, pie, yarda;
+    if(valor.includes(",")){
+        valor = valor.replace("," , ".");
     }
+    if (isNaN(valor)){
+        alert("Se agrego un valor invalido"+id);
+        metro="";
+        pulgada ="";
+        pie ="";
+        yarda="";
+    }else if(id=="metro"){
+       metro = valor;
+       pulgada = 39.3701*valor;
+       pie = 3.28084*valor;
+       yarda = 1.09361*valor;
+    }else if(id=="pulgada"){
+        pulgada = valor;
+        metro = 0.0254*valor;
+        pie = 0.0833333*valor;
+        yarda = 0.0277778*valor;
+    }else if(id=="yarda"){
+        yarda = valor;
+        metro = 0.9144*valor;
+        pulgada = 36*valor;
+        pie = 3*valor;
+    }else if(id=="pie"){
+        pie = valor;
+        metro = 0.3048*valor;
+        pulgada = 12*valor;
+        yarda = 0.333333*valor;
+    }
+    document.lasUnidades.unid_metro.value = Math.round(metro*100)/100 ;
+    document.lasUnidades.unid_pulgada.value = Math.round(pulgada*100)/100 ;
+    document.lasUnidades.unid_pie.value = Math.round(pie*100)/100;
+    document.lasUnidades.unid_yarda.value = Math.round(yarda*100)/100;
 }
 function convertirGR(id) {
     var grad,rad;
@@ -45,7 +57,7 @@ function convertirGR(id) {
 }
 
 function mostrar_ocultar(valorMO) {
-    if (valorMO=="val_ocultar"){
+    if (valorMO=="val_mostrar"){
         document.getElementById ("divMO") .style.display = 'block';
     }else if (valorMO=="val_ocultar"){
         document.getElementById ("divMO").style.display = 'none';
@@ -56,5 +68,25 @@ function calcularSuma() {
     var num1,num2;
     num1=Number (document.getElementsByName("sum_num1") [0].value);
     num2=Number(document.getElementsByName("sum_num2") [0].value);
-    document.getElementsByName("sum_total" ) [0].value= num1 + num2;
+    document.getElementsByName("sum_total" ) [0].innerHTML= num1 + num2;
+}
+function calcularResta() {
+    var num1,num2;
+    num1=Number (document.getElementsByName("res_num1") [0].value);
+    num2=Number(document.getElementsByName("res_num2") [0].value);
+    document.getElementsByName("res_total" ) [0].innerHTML= num1 - num2;
+}
+
+function calcularMulti() {
+    var num1,num2;
+    num1=Number (document.getElementsByName("mul_num1") [0].value);
+    num2=Number(document.getElementsByName("mul_num2") [0].value);
+    document.getElementsByName("mul_total" ) [0].innerHTML= num1 * num2;
+}
+
+function calcularDiv() {
+    var num1,num2;
+    num1=Number (document.getElementsByName("div_num1") [0].value);
+    num2=Number(document.getElementsByName("div_num2") [0].value);
+    document.getElementsByName("div_total" ) [0].innerHTML= num1 / num2;
 }
