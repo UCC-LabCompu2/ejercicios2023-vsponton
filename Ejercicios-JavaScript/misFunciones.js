7c326bd7f086a5ec4eab9f21823d94ce3e86fec0
vsponton <2146222@ucc.edu.ar>
2023-05-28 11:27:38 -0300

    Se realizaron ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -90,3 +90,48 @@ function calcularDiv() {
     num2=Number(document.getElementsByName("div_num2") [0].value);
     document.getElementsByName("div_total" ) [0].innerHTML= num1 / num2;
 }
+
+function cargarWeb() {
+    var cant,unidad,urlComp;
+
+    cant = document.getElementById("distancia").value;
+    unidad = document.getElementsByName("unidades")[0].value;
+    urlComp = "segundaWeb.html#" + cant + "#" + unidad;
+    window.open(urlComp);
+}
+function cargarResultado() {
+    var urlComp,can,un;
+    urlcomp = window.location.href.split("/")[5];
+    can = urlComp.split("#")[1];
+    un = urlComp.split("#")[2];
+    document.getElementById("dist").value = can + " " + un;
+}
+
+function guardadlocalStorage() {
+    let distancia,unidad;
+    distancia = document.getElementById("distancia").value;
+    unidad = document.getElementsByName("unidades") [0].value;
+    localStorage.setItem("distanciaLS",distancia);
+    localStorage.setItem("unidadesLS",unidad);
+    window.open("2_Web.html")
+}
+
+function cargarLocalStorage() {
+    let cant,un;
+    cant = localStorage.getItem("distanciaLS");
+    un = localStorage.getItem("unidadesLS");
+    document.getElementById("dist").value = cant + " " + un;
+}
+function dibujarCirCuad() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var yMax = canvas.height;
+    var margen = 5;
+    var xMax = canvas.width;
+    ctx.fillStyle = "#333899";
+    ctx.fillRect(0+margen,yMax-40,40,40);
+    ctx.arc(xMax/2,yMax/2,20,0,2*Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "8b4c99";
+    ctx.fill();
+}
